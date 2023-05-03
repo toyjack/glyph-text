e39faae0ae21d9706ad8d55edc7a4dd05d11d112
toyjack <toyjack@gmail.com>
2023-05-03 23:36:34 +0900

    update: menu

--- components/Header.tsx
+++ components/Header.tsx
@@ -101,7 +101,7 @@ function Header() {
             <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
             <AlertDialogDescription>
               This action cannot be undone. This will permanently delete your
-              account and remove your data from our servers.
+              file.
             </AlertDialogDescription>
           </AlertDialogHeader>
           <AlertDialogFooter>
